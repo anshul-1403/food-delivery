e247b314183150ab4c73f0237f85753e52278894
Anshul <darkhunter1432004@gmail.com>
2025-03-16 22:34:22 +0530

    bug error

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -15,7 +15,7 @@ const Shop = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`https://food-delivery-1403.vercel.app/api/dishes`);
+        const response = await axios.get(`https://food-delivery-143.vercel.app/api/dishes`);
         console.log(response.data);
         setProduct(response.data);
         setFoodOnly(response.data.filter((item) => item.category === "food"));
